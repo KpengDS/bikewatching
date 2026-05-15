2c2e24780015075b49b9b4cc4dc29dd214a7dcbd
KpengDS <k2peng@ucsd.edu>
2026-05-15 15:31:35 -0700

    s

--- map.js
+++ map.js
@@ -1,20 +1,82 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-// YOUR TOKEN HERE
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3BlbmdjYyIsImEiOiJjbXA3YXlwaDQwNHZuMnJxMGZtenpjNXB1In0.gl_r17MjMR9V9hXpkRBdog';
 
 const map = new mapboxgl.Map({
   container: 'map',
-
   style: 'mapbox://styles/mapbox/streets-v12',
-
   center: [-71.09415, 42.36027],
-
   zoom: 12,
-
   minZoom: 5,
-
   maxZoom: 18,
 });
 
-console.log('Mapbox loaded:', mapboxgl);
+map.on('load', async () => {
+  const bikeLaneStyle = {
+    'line-color': '#32D400',
+    'line-width': 3,
+    'line-opacity': 0.4,
+  };
+
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: bikeLaneStyle,
+  });
+
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
+
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: bikeLaneStyle,
+  });
+
+  const jsonURL = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+  const jsonData = await d3.json(jsonURL);
+  const stations = jsonData.data.stations;
+
+  const svg = d3.select('#map').select('svg');
+
+  function getCoords(station) {
+    const point = new mapboxgl.LngLat(+station.lon, +station.lat);
+    const { x, y } = map.project(point);
+
+    return { cx: x, cy: y };
+  }
+
+  const circles = svg
+    .selectAll('circle')
+    .data(stations)
+    .enter()
+    .append('circle')
+    .attr('r', 5)
+    .attr('fill', 'steelblue')
+    .attr('stroke', 'white')
+    .attr('stroke-width', 1)
+    .attr('opacity', 0.8);
+
+  function updatePositions() {
+    circles
+      .attr('cx', (d) => getCoords(d).cx)
+      .attr('cy', (d) => getCoords(d).cy);
+  }
+
+  updatePositions();
+
+  map.on('move', updatePositions);
+  map.on('zoom', updatePositions);
+  map.on('resize', updatePositions);
+  map.on('moveend', updatePositions);
+});
